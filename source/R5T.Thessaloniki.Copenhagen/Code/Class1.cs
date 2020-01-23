69943abd6f77997a7b91da5214cb370b67a32adc
MinexAutomation <minex.automation@gmail.com>
2020-01-22 17:04:55 -0800

    Added CDriveTemporaryDirectoryPathProvider implementation of the ITemporaryDirectoryPathProvider service.

--- source/R5T.Thessaloniki.Copenhagen/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Thessaloniki.Copenhagen
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Thessaloniki.Copenhagen/Code/Services/Implementations/CDriveTemporaryDirectoryPathProvider.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using R5T.Copenhagen;
+using R5T.Lombardy;
+using R5T.Troy;
+
+
+namespace R5T.Thessaloniki.Copenhagen
+{
+    public class CDriveTemporaryDirectoryPathProvider : ITemporaryDirectoryPathProvider
+    {
+        private ICDrivePathProvider CDrivePathProvider { get; }
+        private ITemporaryDirectoryNameConvention TemporaryDirectoryNameConvention { get; }
+        private IStringlyTypedPathOperator StringlyTypedPathOperator { get; }
+
+
+        public CDriveTemporaryDirectoryPathProvider(
+            ICDrivePathProvider cDrivePathProvider,
+            ITemporaryDirectoryNameConvention temporaryDirectoryNameConvention,
+            IStringlyTypedPathOperator stringlyTypedPathOperator)
+        {
+            this.CDrivePathProvider = cDrivePathProvider;
+            this.TemporaryDirectoryNameConvention = temporaryDirectoryNameConvention;
+            this.StringlyTypedPathOperator = stringlyTypedPathOperator;
+        }
+
+        public string GetTemporaryDirectoryPath()
+        {
+            var cDrivePath = this.CDrivePathProvider.GetCDrivePath();
+
+            var temporaryDirectoryName = this.TemporaryDirectoryNameConvention.GetTemporaryDirectoryName();
+
+            var temporaryDirectoryPath = this.StringlyTypedPathOperator.GetDirectoryPath(cDrivePath, temporaryDirectoryName);
+            return temporaryDirectoryPath;
+        }
+    }
+}
