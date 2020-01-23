9f9a37982539484e2ef2256ec5fa269343b46451
MinexAutomation <minex.automation@gmail.com>
2020-01-22 16:57:32 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Thessaloniki.Copenhagen.Testing/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace R5T.Thessaloniki.Copenhagen.Testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ source/R5T.Thessaloniki.Copenhagen/Code/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace R5T.Thessaloniki.Copenhagen
+{
+    public class Class1
+    {
+    }
+}
