580296f8582826bd8b13d7193da40ea1b98fbaef
MinexAutomation <minex.automation@gmail.com>
2020-01-30 01:06:02 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.Thessaloniki.Copenhagen/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Copenhagen;
+using R5T.Dacia;
+using R5T.Lombardy;
+using R5T.Troy;
+
+
+namespace R5T.Thessaloniki.Copenhagen
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="CDriveTemporaryDirectoryPathProvider"/> implementation of <see cref="ITemporaryDirectoryPathProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddCDriveTemporaryDirectoryPathProvider(this IServiceCollection services,
+            ServiceAction<ICDrivePathProvider> addCDrivePathProvider,
+            ServiceAction<ITemporaryDirectoryNameConvention> addTemporaryDirectoryNameConvention,
+            ServiceAction<IStringlyTypedPathOperator> addStringlyTypedPathOperator)
+        {
+            services
+                .AddSingleton<ITemporaryDirectoryPathProvider, CDriveTemporaryDirectoryPathProvider>()
+                .RunServiceAction(addCDrivePathProvider)
+                .RunServiceAction(addTemporaryDirectoryNameConvention)
+                .RunServiceAction(addStringlyTypedPathOperator)
+                ;
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="CDriveTemporaryDirectoryPathProvider"/> implementation of <see cref="ITemporaryDirectoryPathProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static ServiceAction<ITemporaryDirectoryPathProvider> AddCDriveTemporaryDirectoryPathProviderAction(this IServiceCollection services,
+            ServiceAction<ICDrivePathProvider> addCDrivePathProvider,
+            ServiceAction<ITemporaryDirectoryNameConvention> addTemporaryDirectoryNameConvention,
+            ServiceAction<IStringlyTypedPathOperator> addStringlyTypedPathOperator)
+        {
+            var serviceAction = new ServiceAction<ITemporaryDirectoryPathProvider>(() => services.AddCDriveTemporaryDirectoryPathProvider(
+                addCDrivePathProvider,
+                addTemporaryDirectoryNameConvention,
+                addStringlyTypedPathOperator));
+            return serviceAction;
+        }
+    }
+}
